0486c041cfe390b621a205e4a6e55320bc4fccf9
Anthony <anthony_rose12@hotmail.com>
2017-12-31 18:55:29 -0500

    will couples stay together almost done. Moving to the server because I need more power

---
+++ DataScienceUtils/__init__.py
@@ -0,0 +1,19 @@
+# Collection of Data Science Utilities. If it gets out of hand break them off into their on files
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class GetSubsetTransform(BaseEstimator, TransformerMixin):
+    """
+    Selects the columns of a numpy array.
+    This allows subsets of X in a pipeline
+    """
+    def __init__(self, indices):
+        self.indices = indices
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return np.array(X)[:, self.indices]
